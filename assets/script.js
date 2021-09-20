147ff7cfa0a658d695ea52c52a9d6d9f18493e68
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-09-20 12:58:26 -0500

    adding links to pages

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,25 @@
-const pagesArray = [
-  "github.html",
-  "portfolio.html",
-  "resume.html",
-  "bio.html",
-  "contact.html",
-];
+// const pagesArray = [
+//   "github.html",
+//   "portfolio.html",
+//   "resume.html",
+//   "bio.html",
+//   "contact.html",
+// ];
 
-var nextPage = function (pagesArray) {
-  for (var i = 0; i < pagesArray.length; i++) {
-    document
-      .querySelector(`#stripe${i}`)
-      .addEventListener("click", openNewPage);
+// var nextPage = function (pagesArray) {
+//   for (var i = 0; i < pagesArray.length; i++) {
+//     document
+//       .querySelector(`#stripe${i}`)
+//       .addEventListener("click", openNewPage);
 
-    function openNewPage(pagesArray) {
-      window.open(`${i}`);
+//     function openNewPage(pagesArray) {
+//       window.open(`${i}`);
 
-      //   window.location.replace(pagesArray[i]);
-    }
-  }
-};
-nextPage(pagesArray);
+//       //   window.location.replace(pagesArray[i]);
+//     }
+//   }
+// };
+// nextPage(pagesArray);
 
-const deezNutz = ["nut1", "nut2"];
-console.log(deezNutz);
+// const deezNutz = ["nut1", "nut2"];
+// console.log(deezNutz);
